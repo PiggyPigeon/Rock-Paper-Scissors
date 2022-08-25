9f97e9e57f4c08928def86e527e92deabf4b8ae8
“Bethany <Bethanygellington@gmail.com>
2022-08-25 17:06:22 -0400

    add results to game

--- intermediate-rock-paper-scissors/script.js
+++ intermediate-rock-paper-scissors/script.js
@@ -1,22 +1,25 @@
-const userChoiceDisplay = document.createElement('h1')
-const computerChoiceDisplay = document.createElement('h1')
-const resultDisplay = document.createElement('h1')
-const gameGrid = document.getElementById('game')
+const userChoiceDisplay = document.createElement("h1")
+const computerChoiceDisplay = document.createElement("h1")
+const resultDisplay = document.createElement("h1")
+const gameGrid = document.getElementById("game")
 gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay)
 //h1 tags not showing in console :,-(
 const choices = ['rock', 'paper', 'scissors']
 let userChoice
+let computerChoice
 
 const handleClick = (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = "User choice: " + userChoice
     generateComputerChoice()
+    getResults()
 }
 
 //randomly generates computer choice, passes it through choices array to assign string
 const generateComputerChoice = () => {
     const randomChoice = choices[Math.floor(Math.random() * choices.length)]
-    computerChoiceDisplay.innerHTML = "Computer choice: " + randomChoice
+    computerChoice = randomChoice
+    computerChoiceDisplay.innerHTML = "Computer choice: " + computerChoice
 }
 
 //add the buttons. not sure i understand what is going on here...
@@ -29,3 +32,22 @@ for (let i = 0; i < choices.length; i++) {
     gameGrid.appendChild(button)
 }
 
+const getResults = () => {
+    switch (userChoice + computer) {
+        case 'scissorspaper':
+        case 'rockscissors':
+        case 'paperrock':
+            resultDisplay.innerHTML = "You win!"
+            break
+        case 'paperscissors':
+        case 'scissorsrock':
+        case 'rockpaper':
+            resultDisplay.innerHTML = "You lose!"
+            break
+        case 'scissorsscissors':
+        case 'rockrock':
+        case 'paperpaper':
+            resultDisplay.innerHTML = "It\'s a tie..."
+            break
+    }
+}
